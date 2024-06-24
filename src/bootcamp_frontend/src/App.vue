f9cf4e79100e66e033d470536d718a3a61929019
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-24 18:38:58 +0200

    Add dfx vue project

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,13 @@
+
+
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    Płynie w rzece woda prędko <br>
+    Paweł tkwi na brzegu z wędką <br> <br>
+    Siedzi, czeka, w wodę patrzy <br>
+    Czy już ryba chwyta haczyk <br> <br>
+    Wciąż w bezruchu tkwi jak grzyb <br>
+    Bo w tej rzece nie ma ryb
+  </main>
+</template>
